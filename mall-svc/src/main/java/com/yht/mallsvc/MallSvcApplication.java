0368c6d6bfee4cb62b82621e9f9bd2b7e6f11aa2
yanghaitao <yhaitao@aliyun.com>
2023-05-30 11:26:55 +0800

    init

---
+++ mall-svc/src/main/java/com/yht/mallsvc/MallSvcApplication.java
@@ -0,0 +1,13 @@
+package com.yht.mallsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSvcApplication.class, args);
+    }
+
+}
